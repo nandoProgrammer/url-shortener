f10903aec7ff6c2396f5a2d56bd708db9aac6674
devfodex <nandinhop.costa@gmail.com>
2021-08-18 10:36:15 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://admin:admin@teste-dio-url-shortener.qvyzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
